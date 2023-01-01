bf1906cc46ccab04045034470905469c9ce07c2a
Fakiri <ismaiilfakirii@gmail.com>
2023-01-01 18:54:29 +0100

    update functions to async

--- api/_tests/repository/movie/test_movie.py
+++ api/_tests/repository/movie/test_movie.py
@@ -2,13 +2,13 @@ import pytest
 
 from api.entities.movie import Movie
 from api.repository.movie.abstractions import RepositoryException
-from api.repository.movie.memory import MemoryMovieRepository
+from api.repository.movie.movie_memory import MemoryMovieRepository
 
 
 @pytest.fixture(scope="module")
 def movie_1():
     return Movie(
-        movie_id="movie1",
+        _id="movie1",
         title="365 Days",
         description="A woman falls victim to a dominant mafia boss, who imprisons her and gives her one year to fall in love with him",
         year=2020,
@@ -16,44 +16,51 @@ def movie_1():
     )
 
 
-def test_create(movie_1):
+@pytest.mark.asyncio
+async def test_create(movie_1):
     repo = MemoryMovieRepository()
-    repo.create(movie_1)
-    assert repo.get_by_id(movie_id="movie1") is movie_1
+    await repo.create(movie_1)
+    assert await repo.get_by_id(movie_id="movie1") is movie_1
 
 
 @pytest.mark.parametrize("movies, movie_id, excepted_movie", [([], "movie1", None)])
-def test_get_by_id(movies, movie_id, excepted_movie):
+@pytest.mark.asyncio
+async def test_get_by_id(movies, movie_id, excepted_movie):
     repo = MemoryMovieRepository()
     for movie in movies:
-        repo.create(movie)
-    assert repo.get_by_id(movie_id) == excepted_movie
+        await repo.create(movie)
+    assert await repo.get_by_id(movie_id) == excepted_movie
 
 
 @pytest.mark.parametrize("movies, movie_title, excepted_movies", [([], "365 Days", [])])
-def test_get_by_title(movies, movie_title, excepted_movies):
+@pytest.mark.asyncio
+async def test_get_by_title(movies, movie_title, excepted_movies):
     repo = MemoryMovieRepository()
     for movie in movies:
-        repo.create(movie)
-    assert repo.get_by_title(movie_title) == excepted_movies
+        await repo.create(movie)
+    assert await repo.get_by_title(movie_title) == excepted_movies
 
 
-def test_update(movie_1):
+@pytest.mark.asyncio
+async def test_update(movie_1):
     repo = MemoryMovieRepository()
-    repo.create(movie_1)
-    repo.update(movie_id="movie1", update_param={"watched": False})
-    assert repo.get_by_id("movie1").watched == False
+    await repo.create(movie_1)
+    await repo.update(movie_id="movie1", update_param={"watched": False})
+    movie = await repo.get_by_id("movie1")
+    assert movie.watched == False
 
 
-def test_update_fail(movie_1):
+@pytest.mark.asyncio
+async def test_update_fail(movie_1):
     repo = MemoryMovieRepository()
-    repo.create(movie_1)
+    await repo.create(movie_1)
     with pytest.raises(RepositoryException):
-        repo.update(movie_id="movie1", update_param={"id": "movie2"})
+        await repo.update(movie_id="movie1", update_param={"id": "movie2"})
 
 
-def test_delete(movie_1):
+@pytest.mark.asyncio
+async def test_delete(movie_1):
     repo = MemoryMovieRepository()
-    repo.create(movie_1)
-    repo.delete("movie1")
-    assert repo.get_by_id("movie1") is None
+    await repo.create(movie_1)
+    await repo.delete("movie1")
+    assert await repo.get_by_id("movie1") is None
--- api/repository/movie/abstractions.py
+++ api/repository/movie/abstractions.py
@@ -9,32 +9,32 @@ class RepositoryException(Exception):
 
 
 class MovieRepository(abc.ABC):
-    def create(self, movie: Movie):
+    async def create(self, movie: Movie):
         """
         INsert a movie to the database and return True on success.
         Raises RepositoryException if failure.
         """
         raise NotImplementedError
 
-    def get_by_id(self, movie_id: str) -> typing.Optional[Movie]:
+    async def get_by_id(self, movie_id: str) -> typing.Optional[Movie]:
         """
         Retrieves a movie by his ID and if it's not found return None.
         """
         raise NotImplementedError
 
-    def get_by_title(self, title: str) -> typing.List[Movie]:
+    async def get_by_title(self, title: str) -> typing.List[Movie]:
         """
         Return a list of movies wich share the same title.
         """
         raise NotImplementedError
 
-    def update(self, movie_id: str, update_param: dict):
+    async def update(self, movie_id: str, update_param: dict):
         """
         Update a movie by his ID.
         """
         raise NotImplementedError
 
-    def delete(self, movie_id: str):
+    async def delete(self, movie_id: str):
         """
         Delete a movie by his ID.
         Raises RepositoryException if failure.
