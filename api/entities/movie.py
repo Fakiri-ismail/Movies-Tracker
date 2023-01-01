620deb6e4a6bcc7b0b0205a39cb88890221ea610
Fakiri <ismaiilfakirii@gmail.com>
2023-01-01 18:55:15 +0100

    update movie entity

--- api/entities/movie.py
+++ api/entities/movie.py
@@ -2,31 +2,19 @@ class Movie:
     def __init__(
         self,
         *,
-        movie_id: str,
+        _id: str,
         title: str,
         description: str,
         year: int,
         watched: bool = False
     ) -> None:
-        if not id:
+        if not _id:
             raise ValueError("Movie id is required !")
-        self._id = movie_id
+        self._id = _id
         self._title = title
         self._description = description
         self._year = year
         self._watched = watched
-
-    def __eq__(self, __o: object) -> bool:
-        if not isinstance(__o, Movie):
-            return False
-        else:
-            return (
-                self.id == __o.id
-                and self.title == __o.title
-                and self.description == __o.description
-                and self.year == __o.year
-                and self.watched == __o.watched
-            )
 
     @property
     def id(self) -> str:
@@ -47,3 +35,24 @@ class Movie:
     @property
     def watched(self) -> bool:
         return self._watched
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, Movie):
+            return False
+        else:
+            return (
+                self.id == __o.id
+                and self.title == __o.title
+                and self.description == __o.description
+                and self.year == __o.year
+                and self.watched == __o.watched
+            )
+
+    def to_dict(self) -> dict:
+        return {
+            "_id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "year": self.year,
+            "watched": self.watched,
+        }
