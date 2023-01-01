39a4cb70d6cd4dc7ab7178ec0a4a29c2b0151047
Fakiri <ismaiilfakirii@gmail.com>
2023-01-01 18:51:33 +0100

    rename file

--- api/repository/movie/movie_memory.py
+++ api/repository/movie/movie_memory.py
@@ -1,25 +1,28 @@
 import typing
 
 from api.entities.movie import Movie
-from api.repository.movie.abstractions import (MovieRepository,
-                                               RepositoryException)
+from api.repository.movie.abstractions import MovieRepository, RepositoryException
 
 
 class MemoryMovieRepository(MovieRepository):
+    """
+    MemoryMovieRepository implements the reposetory pattern by using a simple memory dict.
+    """
+
     def __init__(self):
         self._storage = {}
 
-    def create(self, movie: Movie):
+    async def create(self, movie: Movie):
         self._storage[movie.id] = movie
 
-    def get_by_id(self, movie_id: str) -> typing.Optional[Movie]:
+    async def get_by_id(self, movie_id: str) -> typing.Optional[Movie]:
         return self._storage.get(movie_id)
 
-    def get_by_title(self, title: str) -> typing.List[Movie]:
+    async def get_by_title(self, title: str) -> typing.List[Movie]:
         movies = [movie for _, movie in self._storage.items() if movie.title == title]
         return movies
 
-    def update(self, movie_id: str, update_param: dict):
+    async def update(self, movie_id: str, update_param: dict):
         movie = self._storage.get(movie_id)
         if movie:
             for key, value in update_param.items():
@@ -32,5 +35,5 @@ class MemoryMovieRepository(MovieRepository):
         else:
             raise RepositoryException(f"movie {movie_id} not found")
 
-    def delete(self, movie_id: str):
+    async def delete(self, movie_id: str):
         self._storage.pop(movie_id, None)
