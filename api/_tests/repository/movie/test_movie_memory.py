65283e1f3a2dd9503697918e6aa517f9c364e22a
Fakiri <ismaiilfakirii@gmail.com>
2023-01-02 21:11:21 +0100

    add fixture

--- api/_tests/repository/movie/test_movie_memory.py
+++ api/_tests/repository/movie/test_movie_memory.py
@@ -4,9 +4,13 @@ from api.entities.movie import Movie
 from api.repository.movie.abstractions import RepositoryException
 from api.repository.movie.movie_memory import MemoryMovieRepository
 
+@pytest.fixture()
+def repo():
+    return MemoryMovieRepository()
 
-@pytest.fixture(scope="module")
-def movie_1():
+
+@pytest.fixture()
+def movie():
     return Movie(
         _id="movie1",
         title="365 Days",
@@ -17,16 +21,15 @@ def movie_1():
 
 
 @pytest.mark.asyncio
-async def test_create(movie_1):
+async def test_create(repo, movie):
     repo = MemoryMovieRepository()
-    await repo.create(movie_1)
-    assert await repo.get_by_id(movie_id="movie1") is movie_1
+    await repo.create(movie)
+    assert await repo.get_by_id(movie_id="movie1") == movie
 
 
 @pytest.mark.parametrize("movies, movie_id, excepted_movie", [([], "movie1", None)])
 @pytest.mark.asyncio
-async def test_get_by_id(movies, movie_id, excepted_movie):
-    repo = MemoryMovieRepository()
+async def test_get_by_id(repo, movies, movie_id, excepted_movie):
     for movie in movies:
         await repo.create(movie)
     assert await repo.get_by_id(movie_id) == excepted_movie
@@ -34,33 +37,29 @@ async def test_get_by_id(movies, movie_id, excepted_movie):
 
 @pytest.mark.parametrize("movies, movie_title, excepted_movies", [([], "365 Days", [])])
 @pytest.mark.asyncio
-async def test_get_by_title(movies, movie_title, excepted_movies):
-    repo = MemoryMovieRepository()
+async def test_get_by_title(repo, movies, movie_title, excepted_movies):
     for movie in movies:
         await repo.create(movie)
     assert await repo.get_by_title(movie_title) == excepted_movies
 
 
 @pytest.mark.asyncio
-async def test_update(movie_1):
-    repo = MemoryMovieRepository()
-    await repo.create(movie_1)
+async def test_update(repo, movie):
+    await repo.create(movie)
     await repo.update(movie_id="movie1", update_param={"watched": False})
-    movie = await repo.get_by_id("movie1")
-    assert movie.watched == False
+    mv = await repo.get_by_id("movie1")
+    assert mv.watched == False
 
 
 @pytest.mark.asyncio
-async def test_update_fail(movie_1):
-    repo = MemoryMovieRepository()
-    await repo.create(movie_1)
+async def test_update_fail(repo, movie):
+    await repo.create(movie)
     with pytest.raises(RepositoryException):
         await repo.update(movie_id="movie1", update_param={"id": "movie2"})
 
 
 @pytest.mark.asyncio
-async def test_delete(movie_1):
-    repo = MemoryMovieRepository()
-    await repo.create(movie_1)
+async def test_delete(repo, movie):
+    await repo.create(movie)
     await repo.delete("movie1")
     assert await repo.get_by_id("movie1") is None
