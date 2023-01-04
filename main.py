128e73ccfe0e1065a0fbb0be9c48f7be74a9d9f5
Fakiri <ismaiilfakirii@gmail.com>
2023-01-04 19:26:33 +0100

    Update the port number

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from api.api import creat_app
 
 def main():
     app = creat_app()
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
